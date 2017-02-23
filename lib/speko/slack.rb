b1addd4e6b81501f7a1079de3f212a1de180aee8
Kotowick <spencer.kotowick@sage.com>
2017-02-22 22:55:54 -0800

    Updated commenting

--- lib/speko/slack.rb
+++ lib/speko/slack.rb
@@ -3,21 +3,32 @@ require 'speko/slack/client'
 require 'speko/slack/config'
 require 'speko/slack/version'
 
+# All methods are module methods and should be called on the Speko::Slack module.
+#
+# Examples
+#
+#   Speko::Slack.configure
+#   # => {}
 module Speko
   module Slack
     class << self
       require 'slack-ruby-client'
 
+      # Returns the Hash values for configurations settings
       attr_reader :config
 
+      # Returns boolean value - determined if config is set or not
       def configured?
          !!config
       end
 
+      # Returns a Hash of enabled configuration settings.
+      # It will default to: []
       def enabled_configs
         config.try(:enabled_configs) || []
       end
 
+      # Returns a global Config object
       def configure(settings)
         @config = Config.new(settings)
       end
@@ -25,9 +36,11 @@ module Speko
   end
 end
 
+# Configure default settings
 Speko::Slack.configure({
   slack_api_token: ENV['SLACK_API_TOKEN'],
   slack_channel: ENV['SLACK_CHANNEL']
 })
 
+# Return a new Speko::Slack::Client object
 SlackClient = Speko::Slack::Client.new
